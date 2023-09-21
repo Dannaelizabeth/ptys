7305883cf2fd19f305cf1cf2ee13ea45cb2499fa
Dannaelizabeth <dannaelizabethrb@gmail.com>
2023-09-20 22:21:18 -0400

    navbar

---
+++ fs-food/src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+const Navbar = () => {
+  return (
+    <div className={styles.container}>Navbar</div>
+  )
+}
+
+export default Navbar
